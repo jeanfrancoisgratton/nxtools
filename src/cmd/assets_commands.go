e6f50e7d41cf691ace43d750a6d451867528dd9d
jeanfrancoisgratton <jean-francois@famillegratton.net>
2026-03-25 04:45:13 -0400

    Added shorthand flag for directory

--- src/cmd/assets_commands.go
+++ src/cmd/assets_commands.go
@@ -117,7 +117,7 @@ func init() {
 
 	assetsListCmd.Flags().BoolVarP(&assets.LatestAssetsOnly, "latest", "l", false, "Only list the latest version of each logical asset/component")
 	assetsListCmd.Flags().BoolVarP(&assets.AlternateInfo, "alternate", "a", false, "Show alternate asset information")
-	assetsUploadCmd.Flags().StringVar(&assets.UploadDirectory, "directory", "", "Target directory inside the repository (optional; defaults are inferred for raw and yum)")
+	assetsUploadCmd.Flags().StringVarP(&assets.UploadDirectory, "directory", "d", "", "Target directory inside the repository (optional; defaults are inferred for raw and yum)")
 	assetsUploadCmd.Flags().BoolVarP(&assets.ReindexRepo, "reindex", "r", false, "Reindex repo after the upload")
 
 	assetInfoCmd.Flags().BoolVarP(&assets.JsonOutput, "json", "j", false, "Output asset information as JSON")
