4050b5fe4beb859b8937202a88f0637cd6012152
jeanfrancoisgratton <jean-francois@famillegratton.net>
2026-07-18 16:43:44 -0400

    decoupled the version subcommand from COBRA

--- src/cmd/root.go
+++ src/cmd/root.go
@@ -4,21 +4,30 @@
 package cmd
 
 import (
+	"fmt"
 	"os"
 	"runtime"
 	"strings"
 
 	"nxtools/shared"
 
+	hftx "github.com/jeanfrancoisgratton/helperFunctions/v5/terminalfx"
 	"github.com/spf13/cobra"
 )
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:     "nxtools",
-	Short:   "Nexus Repository Manager 3 CLI tool",
-	Version: "1.0.0 (2026.07.18), Go version = v" + strings.TrimPrefix(runtime.Version(), "go"),
-	Long:    `This tools allows you to manage many actions on an NxRM server`,
+	Use:   "nxtools",
+	Short: "Nexus Repository Manager 3 CLI tool",
+	Long:  `This tools allows you to manage many actions on an NxRM server`,
+}
+
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Shows the software version",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println(hftx.White("nxtools v1.0.0 (2026.07.18), Go version = v" + strings.TrimPrefix(runtime.Version(), "go")))
+	},
 }
 
 func Execute() {
@@ -32,7 +41,7 @@ func init() {
 	rootCmd.DisableAutoGenTag = true
 	rootCmd.CompletionOptions.DisableDefaultCmd = true
 
-	rootCmd.AddCommand(envCmd, repoCmd, blobCmd, completionCmd, assetsCmd, assetsUploadCmd, assetsDownloadCmd, assetsComponentInfoCmd, repoReindexCmd, repoMigrateCmd)
+	rootCmd.AddCommand(versionCmd, envCmd, repoCmd, blobCmd, completionCmd, assetsCmd, assetsUploadCmd, assetsDownloadCmd, assetsComponentInfoCmd, repoReindexCmd, repoMigrateCmd)
 
 	rootCmd.Flags().BoolP("version", "V", false, "Show version and exit")
 	rootCmd.PersistentFlags().StringVarP(&shared.Envfile, "env", "e", "defaultEnv.json", "Environment file to load in from $HOME/.config/JFG/nxtools")
