7437c47297913fecc8b8336ad9570a2237179b08
jeanfrancoisgratton <jean-francois@famillegratton.net>
2026-07-20 19:23:50 -0400

    version bump

--- __debian/2.build_binary.sh
+++ __debian/2.build_binary.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-PKGDIR="nxtools-1.0.0-1_amd64"
+PKGDIR="nxtools-1.0.1-1_amd64"
 
 mkdir -p ${PKGDIR}/opt/bin ${PKGDIR}/DEBIAN
 mkdir -p ${PKGDIR}/opt/bin ${PKGDIR}/DEBIAN
--- src/cmd/root.go
+++ src/cmd/root.go
@@ -26,7 +26,7 @@ var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Shows the software version",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println(hftx.White("nxtools v1.0.0 (2026.07.18), Go version = v" + strings.TrimPrefix(runtime.Version(), "go")))
+		fmt.Println(hftx.White("nxtools v1.0.1 (2026.07.20), Go version = v" + strings.TrimPrefix(runtime.Version(), "go")))
 	},
 }
 
